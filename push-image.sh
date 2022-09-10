4d11182ad494f0e747287dd081729cdae7e0fe56
Azeez Salu <aosnotes77@gmail.com>
2022-09-10 02:20:46 -0400

    update

--- push-image.sh
+++ push-image.sh
@@ -6,6 +6,8 @@ set -eu
 # login to your docker hub account
 docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
 
+cd $IMAGE_TAG
+
 # use the docker tag command to give the image a new name
 docker tag $IMAGE_TAG $DOCKER_HUB_REPO_NAME
 
