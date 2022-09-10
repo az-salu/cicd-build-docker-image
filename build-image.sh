03945e9b7c648afb0354d95a9fdbe7bbd328130c
Azeez Salu <aosnotes77@gmail.com>
2022-09-10 01:42:48 -0400

    update

--- build-image.sh
+++ build-image.sh
@@ -4,5 +4,4 @@
 set -eu
 
 # build the docker image
-# docker build -t $IMAGE_TAG -f $IMAGE_TAG/Dockerfile
-docker build -f Dockerfile -t $IMAGE_TAG $IMAGE_TAG
+docker build -f Dockerfile -t $IMAGE_TAG $CODEBUILD_SRC_DIR/$IMAGE_TAG
