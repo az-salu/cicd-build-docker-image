90b6c5e810d5c67c3158e9d5b80437786703525f
Azeez Salu <aosnotes77@gmail.com>
2022-09-10 01:13:57 -0400

    update

--- build-image.sh
+++ build-image.sh
@@ -4,4 +4,5 @@
 set -eu
 
 # build the docker image
-docker build -t $IMAGE_TAG -f $IMAGE_TAG/Dockerfile
+# docker build -t $IMAGE_TAG -f $IMAGE_TAG/Dockerfile
+docker build -f Dockerfile -t $IMAGE_TAG $IMAGE_TAG/
